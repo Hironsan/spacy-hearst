842897fbc8bda6649cbe64f51bc930b8eec2d6e5
Hironsan <hiroki.nakayama.py@gmail.com>
2022-10-11 23:14:29 +0900

    Add such NP as NP pattern

--- hearst/builder.py
+++ hearst/builder.py
@@ -27,3 +27,32 @@ def build_np_such_as_np_patterns(n=5) -> List[List[object]]:
         pattern += [{"POS": {"IN": ["NOUN", "PROPN"]}}]
         patterns.append(pattern)
     return patterns
+
+
+def build_such_np_as_np_patterns(n=5) -> List[List[object]]:
+    """Builds a list of n such as patterns.
+    Such NP as {NP,}* {( and | or )} NP
+
+    Args:
+        n (int): Number of NP patterns to include in the such as pattern.
+
+    Returns:
+        list: List of such as patterns.
+    """
+    patterns = []
+    for i in range(n):
+        pattern = [
+            {"LOWER": "such"},
+            {"POS": {"IN": ["NOUN", "PROPN"]}},
+            {"LOWER": "as"},
+        ]
+        pattern += [
+            {"POS": {"IN": ["NOUN", "PROPN"]}},
+            {"ORTH": ","},
+        ] * i
+        pattern += [] if i == 0 else [{"LOWER": {"IN": ["and", "or"]}}]
+        pattern += [
+            {"POS": {"IN": ["NOUN", "PROPN"]}},
+        ]
+        patterns.append(pattern)
+    return patterns
--- hearst/extractor.py
+++ hearst/extractor.py
@@ -2,21 +2,46 @@ from typing import List, Tuple
 
 from spacy.matcher import Matcher
 
-from .builder import build_np_such_as_np_patterns
+from .builder import build_np_such_as_np_patterns, build_such_np_as_np_patterns
 
 
 class NPSuchAsNPExtractor:
     def __init__(self, nlp, n=5):
         self.nlp = nlp
-        self.nlp.add_pipe("merge_noun_chunks")
         patterns = build_np_such_as_np_patterns(n)
         self.matcher = Matcher(nlp.vocab)
         self.matcher.add("NP such as NP", patterns)
 
     def extract(self, text) -> List[Tuple[str, str]]:
         doc = self.nlp(text)
+        with doc.retokenize() as retokenizer:
+            for span in doc.noun_chunks:
+                retokenizer.merge(span)
         matches = self.matcher(doc, as_spans=True)
         if not matches:
             return []
         noun_chunks = list(max((span for span in matches), key=len).noun_chunks)
         return [(chunk.text, noun_chunks[0].text) for chunk in noun_chunks[1:]]
+
+
+class SuchNPAsNPExtractor:
+    def __init__(self, nlp, n=5):
+        self.nlp = nlp
+        patterns = build_such_np_as_np_patterns(n)
+        self.matcher = Matcher(nlp.vocab)
+        self.matcher.add("such NP as NP", patterns)
+
+    def extract(self, text) -> List[Tuple[str, str]]:
+        doc = self.nlp(text)
+        with doc.retokenize() as retokenizer:
+            for span in doc.noun_chunks:
+                if span[0].lower_ == "such":
+                    continue
+                retokenizer.merge(span)
+        matches = self.matcher(doc, as_spans=True)
+        if not matches:
+            return []
+        match = max((span for span in matches), key=len)
+        hypernym = match[1].text
+        noun_chunks = list(match[3:].noun_chunks)  # skip "such NP as"
+        return [(chunk.text, hypernym) for chunk in noun_chunks]
